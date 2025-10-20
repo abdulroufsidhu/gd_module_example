946225290649dd9af805e5462bfc9d026bf63aff
Abdul Rauf <41958538+abdulroufsidhu@users.noreply.github.com>
2025-10-20 18:23:16 +0500

    Add files via upload

---
+++ entrypoint.gd
@@ -0,0 +1,15 @@
+extends Node
+
+func start():
+    var popup = Window.new()
+    popup.title = "Example Module"
+    popup.size = Vector2(300, 150)
+
+    var label = Label.new()
+    label.text = "Hello from Example Module!"
+    label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+    label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+    popup.add_child(label)
+
+    get_tree().root.add_child(popup)
+    popup.popup_centered()
