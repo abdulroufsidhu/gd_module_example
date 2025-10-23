2e994da4fd94065ec22f31dab2a19a10c22269e1
Abdul Rauf <41958538+abdulroufsidhu@users.noreply.github.com>
2025-10-23 23:02:27 +0500

    Update entrypoint.gd

--- entrypoint.gd
+++ entrypoint.gd
@@ -9,6 +9,10 @@ func start():
     label.text = "Hello from Example Module!"
     label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
     label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+    popup.close_requested.connect(func():
+        popup.queue_free()
+    )
+
     popup.add_child(label)
 
     get_tree().root.add_child(popup)
